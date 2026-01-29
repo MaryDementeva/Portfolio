1b5a88a1694ed927dd88735d49b6b3041fde926d
MaryDementeva <mbrovarova@mail.ru>
2026-01-29 14:52:24 +0400

    Update model.py

--- model.py
+++ model.py
@@ -19,30 +19,6 @@ device
 #путь к файлу конфигурации набора дополненных данных
 yaml_path = r'/content/drive/MyDrive/NN_diplom/full_dataset/data_add.yaml'
 
-"""#Обучение модели **yolo11s** на изображениях 64x64 с возобновлением обучения"""
-
-#инициализируем модель
-Yolo_model11s = YOLO('yolo11s.pt')
-
-Yolo_model11s.info()
-
-#начальные настройки обучения - ниже обучение ниже возобновленное обучение на промежуточных сохраненных параметрах модели
-results = Yolo_model11s.train(data = yaml_path, epochs = 10, imgsz = 128, device= device)
-
-#архивируем папку runs с результатами обучения в архив runs_yolo
-!zip -r /content/runs_yolo_11s /content/runs
-files.download(r'/content/runs_yolo_11s.zip')
-
-"""#Возобновление обучения"""
-
-#возобновление обучение на ранее сохраненных промежуточных контрольных точках
-resumed_yolo11s = YOLO(r'/content/yolo11s_64.pt') #загрузила в content последнюю сподсчитанную ранее модель
-results = resumed_yolo11s.train(data = yaml_path, epochs = 10, imgsz = 64, device= device, resume=True)
-#resume=True - добавить при вызове с перерывом обучения
-
-#архивируем папку runs с результатами обучения в архив runs_yolo
-!zip -r /content/runs_yolo_11s /content/runs
-files.download(r'/content/runs_yolo_11s.zip')
 
 """#Обучение yolo11s с параметрами отличными от настроек по умолчанию, но с размером изображения как в обученной выше модели yolo11n  128x128    
 patience = 2 - количество эпох ожидания без улучшения метрик проверки   
@@ -54,7 +30,7 @@ classes = [1,2] - только классы люди и владельцы
 dropout = 0.8 - для исключения переобучения
 """
 
-#начальные настройки обучения - ниже обучение ниже возобновленное обучение на промежуточных сохраненных параметрах модели
+#начальные настройки обучения - ниже возобновленное обучение на промежуточных сохраненных весах модели
 results = Yolo_model11s.train(data = yaml_path, epochs = 10, imgsz = 128, device= device, patience = 2, batch = 32, save = True, save_period = 1, pretrained = True, classes = [0,2], dropout = 0.8)
 
 #архивируем папку runs с результатами обучения в архив runs_yolo
